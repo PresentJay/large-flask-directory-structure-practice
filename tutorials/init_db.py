d224a1188b1521d28a1957e91cf22eb39fc58c3c
정현재 <presentj94@gmail.com>
2023-02-07 10:56:24 +0900

    feat: using postgresql 튜토리얼 추가

---
+++ tutorials/init_db.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import psycopg2
+
+load_dotenv(find_dotenv())
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_NAME"),
+    database=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute("DROP TABLE IF EXISTS books;")
+cur.execute(
+    "CREATE TABLE books (id serial PRIMARY KEY,"
+    "title varchar (150) NOT NULL,"
+    "author varchar (50) NOT NULL,"
+    "pages_num integer NOT NULL,"
+    "review text,"
+    "date_added date DEFAULT CURRENT_TIMESTAMP);"
+)
+
+# Insert data into the table
+
+cur.execute(
+    "INSERT INTO books (title, author, pages_num, review)" "VALUES (%s, %s, %s, %s)",
+    ("A Tale of Two Cities", "Charles Dickens", 489, "A great classic!"),
+)
+
+
+cur.execute(
+    "INSERT INTO books (title, author, pages_num, review)" "VALUES (%s, %s, %s, %s)",
+    ("Anna Karenina", "Leo Tolstoy", 864, "Another great classic!"),
+)
+
+conn.commit()
+
+cur.close()
+conn.close()
